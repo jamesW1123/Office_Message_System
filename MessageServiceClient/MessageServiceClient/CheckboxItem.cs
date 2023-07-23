cffd7208ccb83ab7a474efd617f9999581c776a9
jamesw1123 <jamesw11233@gmail.com>
2023-07-23 08:14:25 -0500

    Added a class for check box items to handle recipients and senders.

---
+++ MessageServiceClient/MessageServiceClient/CheckboxItem.cs
@@ -0,0 +1,14 @@
+﻿namespace MessageServiceClientApp
+{
+    public class CheckboxItem
+    {
+        public string DisplayName { get; set; }
+        public string UserName { get; set; }
+
+        public CheckboxItem(string displayname, string username)
+        {
+            DisplayName = displayname;
+            UserName = username;
+        }
+    }
+}
