f115be12fd496cf5b5652b527e4ee9b6ecb1a1b9
jamesw1123 <jamesw11233@gmail.com>
2023-07-23 08:19:48 -0500

    Added a class to help keeping track of individual messages.

---
+++ MessageServiceClient/MessageServiceClient/Node.cs
@@ -0,0 +1,14 @@
+﻿namespace MessageServiceClientApp
+{
+    public class Node
+    {
+        public int Key { get; set; }
+        public MService.Message Value { get; set; }
+
+        public Node(int key, MService.Message value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
